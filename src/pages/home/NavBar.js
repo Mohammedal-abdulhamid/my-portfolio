df92294b2ff022b54602a512f27609e97d681345
Macair <macair@Macs-MacBook-Air.local>
2023-02-18 23:03:25 +0000

    set out route to pages

---
+++ src/pages/home/NavBar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { NavLink } from 'react-bootstrap';
+import { Navbar, Nav } from "react-bootstrap";
+
+function NavBar(){
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/">My Portfolio</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
+          <Nav.Link as={Link} to="/about">About</Nav.Link>
+          <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+          <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    // <div bg="light" expand="lg" >
+    //   <Link as={Link} to="/">
+    //     Home
+    //   </Link>
+    //   <Link as={Link} to="/about">
+    //     About
+    //   </Link>
+    //   <Link as={Link} to="/projects">
+    //     Projects
+    //   </Link>
+    //   <Link as={Link} to="/contact">
+    //     Contact
+    //   </Link>
+    // </div>
+  );
+  }
+
+export default NavBar;
