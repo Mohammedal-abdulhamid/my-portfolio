618ce8694546f59d5187062c1af615a8ee7e7ed3
Macair <macair@Macs-MacBook-Air.local>
2023-02-19 00:44:44 +0000

    home page test and photo

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,26 @@
-import React from 'react'
-
-
-function Home() {
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import myPhoto from'./Photo.jpg';
+const Home = () => {
   return (
+    <Container className="my-5">
+      <Row className="align-items-center">
+        <Col md={4}>
+          <img src = {myPhoto} alt='My photo'rounded fluid width = '300' />
+        </Col>
+        <Col md={8}>
+          <h1 className="display-4 mb-4">Hello, I'm Mohammed</h1>
+          <p className="lead">
+            I'm a full-stack developer based in Manchester,UK. I have a passion for coding and creating beautiful, responsive, and user-friendly websites and applications.
+          </p>
+          <p>
+            In my free time, I enjoy traveling, and spending time with my family and friends. Feel free to contact me for any inquiries or collaborations.
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-    <div>
-     Home
-    </div>
-  )
-}
+export default Home;
 
-export default Home
+
