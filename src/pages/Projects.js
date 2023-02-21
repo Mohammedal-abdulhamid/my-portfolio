bdec06695aabd7a7e7b7c40a60253e3ed77a1328
Macair <macair@Macs-MacBook-Air.local>
2023-02-21 23:46:36 +0000

    adding style to the project card

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,57 @@
 import React from 'react'
-
+import './Project.css'
 function Projects() {
   return (
-    <div>Projects</div>
+    // <div className="vh-100 d-flex container-fluid vh-100 d-flex flex-column justify-content-center align-items-center bg-dark text-white">
+    // HTML code for the project section 
+    <section id="projects" class="bg-dark text-light py-5">
+      <div class="container">
+        <h2 class="text-center mb-5">Projects</h2>
+    
+         {/* Project cards  */}
+        <div class="row">
+    
+          {/* Card for the Shop Comments App */}
+          <div class="col-md-6 col-lg-4 mb-4">
+            <div class="card border-light">
+              <div class="card-body">
+                <h5 class="card-title text-primary">Shop Comments App</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Handlebars, MongoDB, Heroku</h6>
+                <ul class="card-text">
+                  <li class ="projectText text-muted ">Built the database from scratch with MongoDB and connected it to the front-end.</li>
+                  <li class ="projectText text-muted ">Made the app dynamic and added different routes for each product.</li>
+                  <li class ="projectText text-muted ">Users’ inputs were saved for each product and displayed.</li>
+                  <li class ="projectText text-muted ">Applied dynamic styling to the top-rated product and showed it at the top of the page.</li>
+                </ul>
+                <a href="https://github.com/Mohammedal-abdulhamid/group-project-shop-comments" class="card-link text-primary" target="_blank">View on GitHub</a>
+              </div>
+            </div>
+          </div>
+    
+          {/* Card for Caritas Education */}
+          <div class="col-md-6 col-lg-4 mb-4">
+            <div class="card border-light">
+              <div class="card-body">
+                <h5 class="card-title text-primary">Caritas Education</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Gatsby, Contentful, TailWind, GraphQL, Jest, Figma, Netlify</h6>
+                <ul class="card-text">
+                  <li class ="projectText text-muted">Set a daily stand-up and task distribution.</li>
+                  <li class ="projectText text-muted " >Worked with a team to design the layout of the webpage using Wireframe.</li>
+                  <li class ="projectText text-muted  ">Set meetings with the customer at each stage of the work to get his feedback.</li>
+                  <li class ="projectText text-muted ">Coded the testing part using Jest Testing Library and carried out some debugging.</li>
+                </ul>
+                <a href="https://github.com/Mohammedal-abdulhamid/caritas-final-project" class="card-link text-primary" target="_blank">View on GitHub</a>
+              </div>
+            </div>
+          </div>
+    
+          
+    
+        </div>
+      </div>
+    </section>
+    
+    
   )
 }
 
