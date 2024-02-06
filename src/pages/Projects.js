6bca3a1b49c0494419d1ea1fb42bc8b5f95a2fb1
Mohammedal-abdulhamid <macair@Macs-MacBook-Air.local>
2024-02-06 22:12:13 +0000

    add some styling

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,128 +1,15 @@
-// import React from 'react'
-// import './Project.css'
-// function Projects() {
-//   return (
-//     // <div className="vh-100 d-flex container-fluid vh-100 d-flex flex-column justify-content-center align-items-center bg-dark text-white">
-//     // HTML code for the project section
-//     <section id="projects" class="bg-dark text-light py-5">
-//       <div class="container">
-//         <h2 class="text-center mb-5">Projects</h2>
 
-//          {/* Project cards  */}
-//         <div class="row">
-
-//           {/* Card for the Shop Comments App */}
-//           <div class="col-md-6 col-lg-4 mb-4">
-//             <div class="card border-light">
-//               <div class="card-body">
-//                 <h5 class="card-title text-primary"><a href="https://shop-products-project-440b7e5fb694.herokuapp.com/"> Shop Products App </a></h5>
-//                 <h6 class="card-subtitle mb-2 text-muted">Handlebars, MongoDB, Heroku</h6>
-//                 <ul class="card-text">
-//                   <li class ="projectText text-muted ">Built the database from scratch with MongoDB and connected it to the front-end.</li>
-//                   <li class ="projectText text-muted ">Made the app dynamic and added different routes for each product.</li>
-//                   <li class ="projectText text-muted ">Users’ inputs were saved for each product and displayed.</li>
-//                   <li class ="projectText text-muted ">Applied dynamic styling to the top-rated product and showed it at the top of the page.</li>
-//                 </ul>
-//                 <a href="https://github.com/Mohammedal-abdulhamid/group-project-shop-comments" class="card-link text-primary" target="_blank">View on GitHub</a>
-//               </div>
-//             </div>
-//           </div>
-
-//           {/* Card for Caritas Education */}
-//           <div class="col-md-6 col-lg-4 mb-4">
-//             <div class="card border-light">
-//               <div class="card-body">
-//                 <h5 class="card-title text-primary"><a href="https://caritas-project-b7431297a251.herokuapp.com/">  Caritas</a> </h5>
-//                 <h6 class="card-subtitle mb-2 text-muted">Gatsby, Contentful, TailWind, GraphQL, Jest, Figma, Netlify</h6>
-//                 <ul class="card-text">
-//                   <li class ="projectText text-muted">Set a daily stand-up and task distribution.</li>
-//                   <li class ="projectText text-muted " >Worked with a team to design the layout of the webpage using Wireframe.</li>
-//                   <li class ="projectText text-muted  ">Set meetings with the customer at each stage of the work to get his feedback.</li>
-//                   <li class ="projectText text-muted ">Coded the testing part using Jest Testing Library and carried out some debugging.</li>
-//                 </ul>
-//                 <a href="https://github.com/Mohammedal-abdulhamid/caritas-final-project" class="card-link text-primary" target="_blank">View on GitHub</a>
-//               </div>
-//             </div>
-//           </div>
-//                 {/* Card for Caritas Education */}
-//           <div class="col-md-6 col-lg-4 mb-4">
-//             <div class="card border-light">
-//               <div class="card-body">
-//                 <h5 class="card-title text-primary"> <a href="https://zuhal-website-6f7248444e6d.herokuapp.com/"> Hair & Makeup Salon</a></h5>
-//                 <h6 class="card-subtitle mb-2 text-muted">React, Node js, Express, React routs</h6>
-//                 <ul class="card-text">
-//                   <li class ="projectText text-muted">Set a daily stand-up and task distribution.</li>
-//                   <li class ="projectText text-muted " >Worked with a team to design the layout of the webpage using Wireframe.</li>
-//                   <li class ="projectText text-muted  ">Set meetings with the customer at each stage of the work to get his feedback.</li>
-//                   <li class ="projectText text-muted "> </li>
-//                 </ul>
-//                 <a href="https://github.com/Mohammedal-abdulhamid/Zuhal-website" class="card-link text-primary" target="_blank">View on GitHub</a>
-//               </div>
-//             </div>
-//             </div>
-//              {/* Card for Caritas Education */}
-//           <div class="col-md-6 col-lg-4 mb-4">
-//             <div class="card border-light">
-//               <div class="card-body">
-//                 <h5 class="card-title text-primary"> <a href="https://lighthearted-pixie-af7fc6.netlify.app/"> GitHub User's Search</a></h5>
-//                 <h6 class="card-subtitle mb-2 text-muted"> </h6>
-//                 <ul class="card-text">
-//                   <li class ="projectText text-muted"></li>
-//                   <li class ="projectText text-muted " ></li>
-//                   <li class ="projectText text-muted  "></li>
-//                   <li class ="projectText text-muted "></li>
-//                 </ul>
-//                 <a href="https://github.com/Mohammedal-abdulhamid/github-api-search" class="card-link text-primary" target="_blank">View on GitHub</a>
-//               </div>
-//             </div>
-//             </div>
-//              {/* Card for Caritas Education */}
-//           <div class="col-md-6 col-lg-4 mb-4">
-//             <div class="card border-light">
-//               <div class="card-body">
-//                 <h5 class="card-title text-primary"></h5>
-//                 <h6 class="card-subtitle mb-2 text-muted"></h6>
-//                 <ul class="card-text">
-//                   <li class ="projectText text-muted"></li>
-//                   <li class ="projectText text-muted " ></li>
-//                   <li class ="projectText text-muted  "></li>
-//                   <li class ="projectText text-muted "></li>
-//                 </ul>
-//                 <a href="https://github.com/Mohammedal-abdulhamid/caritas-final-project" class="card-link text-primary" target="_blank">View on GitHub</a>
-//               </div>
-//             </div>
-//             </div>
-//              {/* Card for Caritas Education */}
-//           <div class="col-md-6 col-lg-4 mb-4">
-//             <div class="card border-light">
-//               <div class="card-body">
-//                 <h5 class="card-title text-primary"></h5>
-//                 <h6 class="card-subtitle mb-2 text-muted"></h6>
-//                 <ul class="card-text">
-//                   <li class ="projectText text-muted"></li>
-//                   <li class ="projectText text-muted " ></li>
-//                   <li class ="projectText text-muted  "></li>
-//                   <li class ="projectText text-muted "></li>
-//                 </ul>
-//                 <a href="https://github.com/Mohammedal-abdulhamid/caritas-final-project" class="card-link text-primary" target="_blank">View on GitHub</a>
-//               </div>
-//             </div>
-//             </div>
-
-//         </div>
-//       </div>
-//     </section>
-
-//   )
-// }
-
-// export default Projects
 
 import React from "react";
 import "./Projects.css";
 
 const Projects = () => {
   const shop = [
+    "This App displays all products avaiable in a given shop on the main page",
+    "Technology used are Handlebars,MongoDB,Express and CSS ",
+    "Under each single product the customer can view details of a product and give thier reviews ",
+    "The products are displayed in order starting from the high rated down",
+    "Customer's input saved to the database and used accordingly",
     "Built the database from scratch with MongoDB and connected it to the App",
     "Made the app dynamic and added different routes for each product",
     "Users’ inputs were saved for each product and displayed",
@@ -130,23 +17,29 @@ const Projects = () => {
   ];
 
   const caritas = [
+    "The purpose of this App to help none-technical person to edit the database regularly",
+    " To build an navegatable UI consistent with the existing Caritas webpage",
+    "Technology used includes Gatsby, GraphQl, Wireframe, React Routes, Jest and TailWinds",
     "Set a daily stand-up and task distribution",
     "Worked with a team to design the layout of the webpage using Wireframe",
     "Set meetings with the customer at each stage of the work to get his feedback",
     "Coded the testing part using Jest Testing Library and carried out some debugging",
   ];
   const salon = [
-    "first project to deal with an actual client",
-    "successfully delived on the time and met the client expectation",
-    "full-stack project using reack, Express, Node",
-    "regular team meeting and Agile method approach",
-    "used different styling such as flex box and grid",
+    "First project to deal with an actual client",
+    "Successfully delived on the time and met the client expectation",
+    "Full-stack project using reack, Express, Node",
+    "Regular team meeting and Agile method approach",
+    "Used different styling such as flex box and grid",
+    "Technolog Used Reach, react Routs, Express, CSS"
   ];
   const github_user = [
-    "creating a search UI and apply styling",
-    " user input was dedected and saved",
-    "fetch data that match the used input to desply the tagret used",
-    "displayed the matched used names and their most used language ",
+    
+    "Creating a search UI that enables searching any github user",
+    "Data was fetch from GitHub API and filtered to match the input",
+    "Resuling Data was Filtered again to find the most used language",
+    "Displayed the matched used names and their most used language ",
+    "technology used React CSS"
   ];
   
 
@@ -158,7 +51,7 @@ const Projects = () => {
           <a
             className="project-name"
             href="https://shop-products-project-440b7e5fb694.herokuapp.com/"
-          >
+            target="_blank" rel="noopener noreferrer">
             {" "}
             Shop Products App{" "}
           </a>
@@ -176,7 +69,7 @@ const Projects = () => {
           <a
             className="project-name"
             href="https://caritas-project-b7431297a251.herokuapp.com/"
-          >
+            target="_blank" rel="noopener noreferrer">
             {" "}
             Caritas
           </a>{" "}
@@ -194,7 +87,7 @@ const Projects = () => {
           <a
             className="project-name"
             href="https://zuhal-website-6f7248444e6d.herokuapp.com/"
-          >
+            target="_blank" rel="noopener noreferrer">
             {" "}
             Hair & Makeup Salon
           </a>
@@ -212,7 +105,7 @@ const Projects = () => {
           <a
             className="project-name"
             href="https://lighthearted-pixie-af7fc6.netlify.app/"
-          >
+            target="_blank" rel="noopener noreferrer">
             {" "}
             GitHub User's Search
           </a>
--- src/pages/home/NavBar.js
+++ src/pages/home/NavBar.js
@@ -1,31 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
 // import { NavLink } from 'react-bootstrap';
+import "./NavBar.css";
 import { Navbar, Nav } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+
 function NavBar() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" >
       {/* <Navbar.Brand href="/">My Portfolio</Navbar.Brand> */}
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/">
+        <Nav className="mr-auto" >
+          <Nav.Link  as={Link} to="/"  className="nav-link">
             Home
           </Nav.Link>
-          <Nav.Link as={Link} to="/about">
+          <Nav.Link as={Link} to="/about"  className="nav-link">
             About
           </Nav.Link>
-          <Nav.Link as={Link} to="/projects">
+          <Nav.Link as={Link} to="/projects"  className="nav-link">
             Projects
           </Nav.Link>
-          <Nav.Link as={Link} to="/skills">
+          <Nav.Link as={Link} to="/skills"  className="nav-link">
             Skills
           </Nav.Link>
-          <Nav.Link as={Link} to="/more">
+          <Nav.Link as={Link} to="/more"  className="nav-link">
             More
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
+      <div className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/mohammed-alabdulhamid-445455209/"
+                className="me-4 text-reset"
+              >
+                <FontAwesomeIcon  icon={faLinkedin} />
+              </a>
+              <a
+                href="https://github.com/Mohammedal-abdulhamid"
+                className="me-4 text-reset"
+              >
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+              <a
+                href="mailto:mohammedalabdulhamid@gmail.com"
+                className="me-4 text-reset"
+              >
+                <FontAwesomeIcon icon={faEnvelope} />
+              </a>
+            </div>
     </Navbar>
   );
 }
